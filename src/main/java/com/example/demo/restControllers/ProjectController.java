2fe809c5b54d6cbe97ab0a5db5f6d7699fe2467e
Татьяна <glucharik2@gmail.com>
2025-07-07 21:03:57 +0300

    коммит резюме 6

--- src/main/java/com/example/demo/restControllers/ProjectController.java
+++ src/main/java/com/example/demo/restControllers/ProjectController.java
@@ -4,6 +4,8 @@ import com.example.demo.RepositoryPort;
 import com.example.demo.dto.ProjectDto;
 import com.example.demo.model.Employee;
 import com.example.demo.services.ProjectServices;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -11,15 +13,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
 
+@Tag(name = "Проекты")
+@RestController
 @Controller
 @AllArgsConstructor
 @RequestMapping("/projects")
 public class ProjectController {
     private final ProjectServices service;
+
+    @Operation(summary = "Получить имя по иднетификвтору",
+            description = "Достает из базы данных и преобразует в ДТО данные об employee")
 
     @GetMapping("/{id}")
     public ResponseEntity<ProjectDto> getProjectById(@PathVariable Long id) {
