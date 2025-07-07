fcd3ac69451cbdba076446a875d6fbc3ae618212
Татьяна <glucharik2@gmail.com>
2025-07-07 19:40:15 +0300

    коммит резюме 4

--- src/main/java/com/example/demo/restControllers/ProjectController.java
+++ src/main/java/com/example/demo/restControllers/ProjectController.java
@@ -8,7 +8,6 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ src/main/resources/db/migration/V02__insert-into-employee.sql
@@ -0,0 +1,3 @@
+INSERT INTO Employee (id, name, image, telephon, mail)
+VALUES
+    (5,'Olga','/images/image.jpg',237429344,'olgag@mail.ru');
