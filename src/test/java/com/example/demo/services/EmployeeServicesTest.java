62b27239f21678317345979ac2db2502cadcbbdf
Татьяна <glucharik2@gmail.com>
2025-07-30 18:58:47 +0300

    коммит резюме 10

--- src/test/java/com/example/demo/services/EmployeeServicesTest.java
+++ src/test/java/com/example/demo/services/EmployeeServicesTest.java
@@ -4,6 +4,7 @@ import com.example.demo.dto.EmployeeDto;
 import com.example.demo.mappers.EmployeeMapper;
 import com.example.demo.model.Employee;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,12 +21,12 @@ class EmployeeServicesTest {
     private final Employee EMPLOYEE = new Employee();
     private final EmployeeDto employeeDto = new EmployeeDto();
     private final String EMPLOYEE_EMAIL = "Tata";
-   // @Mock
-   // private EmployeeServices employeeServices;
+    @InjectMocks
+    private EmployeeServices employeeServices;
 
     @Test
     public void getEmployeeByIdTest() {
-        EmployeeServices employeeServices = new EmployeeServices(repository, mapper);
+       // EmployeeServices employeeServices = new EmployeeServices(repository, mapper);
         Mockito.when(repository.findById(EMPLOYEE_ID)).thenReturn(java.util.Optional.of(EMPLOYEE));
         Mockito.when(mapper.toDto(EMPLOYEE)).thenReturn(employeeDto);
         var rez=employeeServices.getEmployeeById(EMPLOYEE_ID);
@@ -33,7 +34,7 @@ class EmployeeServicesTest {
     }
     @Test
     public void getEmployeeByEmailTest() {
-        EmployeeServices employeeServices = new EmployeeServices(repository, mapper);
+      //  EmployeeServices employeeServices = new EmployeeServices(repository, mapper);
         Mockito.when(repository.findByMail(EMPLOYEE_EMAIL)).thenReturn(java.util.Optional.of(EMPLOYEE));
         Mockito.when(mapper.toDto(EMPLOYEE)).thenReturn(employeeDto);
         var rez=employeeServices.getEmployeeByEmail(EMPLOYEE_EMAIL);
