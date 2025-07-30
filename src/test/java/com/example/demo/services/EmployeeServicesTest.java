80ad9189eb1f834ca5f0219ccdda10612d02b3e0
Татьяна <glucharik2@gmail.com>
2025-07-30 18:56:41 +0300

    коммит резюме 9

--- src/test/java/com/example/demo/services/EmployeeServicesTest.java
+++ src/test/java/com/example/demo/services/EmployeeServicesTest.java
@@ -1,23 +1,27 @@
 package com.example.demo.services;
-
 import com.example.demo.SpringDataRepository.EmployeeRepository;
 import com.example.demo.dto.EmployeeDto;
 import com.example.demo.mappers.EmployeeMapper;
 import com.example.demo.model.Employee;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-
-import java.util.List;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class EmployeeServicesTest {
-    private final EmployeeRepository repository = Mockito.mock(EmployeeRepository.class);
-    private final EmployeeMapper mapper = Mockito.mock(EmployeeMapper.class);
+    @Mock
+    private EmployeeRepository repository;
+    @Mock
+    private EmployeeMapper mapper;
     private final Long EMPLOYEE_ID = 1L;
     private final Employee EMPLOYEE = new Employee();
     private final EmployeeDto employeeDto = new EmployeeDto();
     private final String EMPLOYEE_EMAIL = "Tata";
+   // @Mock
+   // private EmployeeServices employeeServices;
 
     @Test
     public void getEmployeeByIdTest() {
