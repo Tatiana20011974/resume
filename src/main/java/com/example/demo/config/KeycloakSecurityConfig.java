12a2099948b08f4db8731d7f8451dbeeb51806e8
Татьяна <glucharik2@gmail.com>
2025-09-29 19:21:45 +0300

    21 keycloak

---
+++ src/main/java/com/example/demo/config/KeycloakSecurityConfig.java
@@ -0,0 +1,40 @@
+import com.example.demo.config.KeyCloakJwtAuthenticationConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class KeycloakSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET,"/resumes").hasRole("constructor")
+                        .requestMatchers(HttpMethod.GET,"/projects").hasRole("meneger")
+                        .requestMatchers(HttpMethod.GET,"/employees").hasAnyRole("resumeRole")
+                        .requestMatchers("/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())
+                        )
+                )
+                .csrf(AbstractHttpConfigurer::disable)  // ← ПОЛНОСТЬЮ ОТКЛЮЧАЕМ CSRF
+                .build();
+    }
+
+
+    @Bean
+    public Converter<Jwt, ? extends AbstractAuthenticationToken> jwtAuthenticationConverter() {
+        var converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(new KeyCloakJwtAuthenticationConverter());
+        return converter;
+    }
+}
--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -2,14 +2,20 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
+/*
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -28,6 +34,23 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
+
         return new BCryptPasswordEncoder();
     }
-}
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable) // Отключение CSRF для API
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/resumes/delete/*").hasRole("ADMIN")
+                        .requestMatchers("/resumes/edit/*").hasAnyRole("ADMIN","USER")
+                        .requestMatchers("/resumes/create/*").hasRole("ADMIN")
+                        .requestMatchers("/**").permitAll()
+
+                )
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .logout(LogoutConfigurer::permitAll)
+                .build();
+    }
+}*/
